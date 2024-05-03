485508689ba0b9422b52481185ac18720b1794e2
Gloria Wekura <wekuraglorian07@gmail.com>
2024-05-03 17:27:29 +0300

    submit code challenge

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,12 @@
+function Transactions({date,description,category,amount}) {
+    return (
+      <tr>
+          <td>{date}</td>
+          <td>{description}</td>
+          <td>{category}</td>
+          <td>{amount}</td>
+      </tr>
+    )
+  }
+  
+  export default Transactions
